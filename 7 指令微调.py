5e996f3a3a820d55096188778a223a2b7a2196a9
wyzlhf <haifeng0130@126.com>
2025-06-05 17:37:57 +0800

    今天（2025年6月5日17:37:31）完成了第七章（最后ollma部分没有弄，留待下次就好）。

--- 7 指令微调.py
+++ 7 指令微调.py
@@ -1,8 +1,31 @@
 import json
 import os
+import re
+import time
+import torch
 import urllib
 import urllib.request
+import tiktoken
+from typing import Dict, List, Tuple
+
+from tqdm import tqdm
 from pydantic import Json
+from functools import partial
+from pydantic.types import AnyType
+from tiktoken import Encoding
+from torch import Tensor, device
+from torch.utils.data import Dataset, DataLoader
+from gpt_download import download_and_load_gpt2
+from config import (
+    GPTModel,
+    load_weights_into_gpt,
+    text_to_token_ids,
+    token_ids_to_text,
+    generate,
+    calc_loss_loader,
+    train_model_simple,
+    plot_losses
+)
 
 
 def download_and_load_file(file_path: str, url: str) -> Json:
@@ -11,9 +34,293 @@ def download_and_load_file(file_path: str, url: str) -> Json:
             text_data = response.read().decode('utf-8')
         with open(file_path, 'w', encoding='utf-8') as file:
             file.write(text_data)
-    else:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            text_data: str = file.read()
     with open(file_path, 'r') as file:
         data: Json = json.load(file)
     return data
+
+
+def format_input(entry: Dict) -> str:
+    instruction_text: str = (
+        f'Below is an instruction that describes a task.'
+        f'Below is an instruction that describes a task.'
+        f'\n\n### Instrucion:\n{entry['instruction']}'
+    )
+    input_text: str = (
+        f'\n\n### Input:\n{entry["input"]}' if entry["input"] else ''
+    )
+    return instruction_text + input_text
+
+
+class InstructionDataset(Dataset):
+    def __init__(self, data: List[Dict[str, str]], tokenizer: Encoding) -> None:
+        self.data: List[Dict[str, str]] = data
+        self.encoded_texts: List[List[int]] = []
+        for entry in data:
+            instruction_plus_input: str = format_input(entry)
+            response_text: str = f'\n\n### Response:\n{entry['output']}'
+            full_text: str = instruction_plus_input + response_text
+            self.encoded_texts.append(tokenizer.encode(full_text))
+
+    def __getitem__(self, index: int) -> list[int]:
+        return self.encoded_texts[index]
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+
+def custom_collate_draft_1(batch: List[list[int]], pad_token_id: int = 50256,
+                           device: device = 'cpu') -> Tensor:
+    batch_max_length: int = max(len(item) + 1 for item in batch)
+    inputs_lst: List[Tensor] = []
+    for item in batch:
+        new_item: List = item.copy()
+        new_item += [pad_token_id]
+        padded: List = (new_item + [pad_token_id] * (batch_max_length - len(new_item)))
+        inputs: Tensor = torch.tensor(padded[:-1])
+        inputs_lst.append(inputs)
+    inputs_tensor: Tensor = torch.stack(inputs_lst).to(device)
+    return inputs_tensor
+
+
+def custom_collate_draft_2(
+        batch: List[List[int]],
+        pad_token_id: int = 50256,
+        device: device = 'cpu') -> Tuple[Tensor, Tensor]:
+    batch_max_length: int = max(len(item) + 1 for item in batch)
+    inputs_lst, targets_lst = [], []
+    for item in batch:
+        new_item: List = item.copy()
+        new_item += [pad_token_id]
+        padded = (new_item + [pad_token_id] * (batch_max_length - len(new_item)))
+        inputs: Tensor = torch.tensor(padded[:-1])
+        targets: Tensor = torch.tensor(padded[1:])
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+    inputs_tensor: Tensor = torch.stack(inputs_lst).to(device)
+    targets_tensor: Tensor = torch.stack(targets_lst).to(device)
+    return inputs_tensor, targets_tensor
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def custom_collate_fn(batch: List[List[int]], pad_token_id: int = 50256, ignore_index: int = -100,
+                      allowed_max_length: int | bool = None, device: device = 'cpu') -> Tuple[Tensor, Tensor]:
+    batch_max_length: int = max(len(item) + 1 for item in batch)
+    inputs_lst, targets_lst = [], []
+    for item in batch:
+        new_item: List = item.copy()
+        new_item += [pad_token_id]
+        padded = (new_item + [pad_token_id] * (batch_max_length - len(new_item)))
+        inputs: Tensor = torch.tensor(padded[:-1])
+        targets: Tensor = torch.tensor(padded[1:])
+        mask: Tensor = torch.tensor(targets == pad_token_id)
+        indices = torch.nonzero(mask).squeeze()
+        if indices.numel() > 1:
+            targets[indices[1:]] = ignore_index
+        if allowed_max_length is not None:
+            inputs = inputs[:allowed_max_length]
+            targets = targets[:allowed_max_length]
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+    inputs_tensor: Tensor = torch.stack(inputs_lst).to(device)
+    targets_tensor: Tensor = torch.stack(targets_lst).to(device)
+    return inputs_tensor, targets_tensor
+
+
+customized_collate_fn = partial(
+    custom_collate_fn,
+    device=device,
+    allowed_max_length=1024,
+)
+
+if __name__ == '__main__':
+    # 7.2 准备用于监督指令微调的数据集
+    file_path = "instruction-data.json"
+    url = (
+        "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch"
+        "/main/ch07/01_main-chapter-code/instruction-data.json"
+    )
+    data: Json | AnyType = download_and_load_file(file_path, url)
+    # # print("Number of entries:", len(data))
+    # print("Example entry:\n", data[0])
+
+    model_input: str = format_input(data[999])
+    desired_response: str = f'\n\n### Response:\n{data[50]['output']}'
+    # print(model_input+desired_response)
+
+    train_portion: int = int(len(data) * 0.85)
+    test_portion: int = int(len(data) * 0.1)
+    val_portion: int = len(data) - train_portion - test_portion
+
+    train_data: Json | AnyType = data[:train_portion]
+    test_data: Json | AnyType = data[train_portion:train_portion + test_portion]
+    val_data: Json | AnyType = data[train_portion + test_portion:]
+
+    # print(f'Training set length: {len(train_data)}')
+    # print(f'Validation set length: {len(val_data)}')
+    # print(f'Test set length: {len(test_data)}')
+    tokenizer = tiktoken.get_encoding('gpt2')
+    # print(tokenizer.encode('<|endoftext|>', allowed_special={'<|endoftext|>'}))
+    inputs_1 = [0, 1, 2, 3, 4]
+    inputs_2 = [5, 6]
+    inputs_3 = [7, 8, 9]
+    batch = [
+        inputs_1,
+        inputs_2,
+        inputs_3
+    ]
+    # print(custom_collate_draft_1(batch))
+    # inputs, targets = custom_collate_draft_2(batch)
+    # print(inputs)
+    # print(targets)
+    inputs, targets = custom_collate_fn(batch)
+    # print(inputs)
+    # print(targets)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # print(device)
+
+    num_workers = 0
+    batch_size = 8
+    torch.manual_seed(123)
+    train_dataset = InstructionDataset(train_data, tokenizer)
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        collate_fn=customized_collate_fn,
+        shuffle=True,
+        drop_last=True,
+        num_workers=num_workers
+    )
+    val_dataset = InstructionDataset(val_data, tokenizer)
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        collate_fn=custom_collate_fn,
+        shuffle=False,
+        drop_last=False,
+        num_workers=num_workers
+    )
+    test_dataset = InstructionDataset(test_data, tokenizer)
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        collate_fn=custom_collate_fn,
+        shuffle=False,
+        drop_last=False,
+        num_workers=num_workers
+    )
+    # for inputs, targets in train_loader:
+    #     print(inputs.shape, targets.shape)
+
+    # 7.5 Loading a pretrained LLM
+    BASE_CONFIG = {
+        "vocab_size": 50257,  # Vocabulary size
+        "context_length": 1024,  # Context length
+        "drop_rate": 0.0,  # Dropout rate
+        "qkv_bias": True  # Query-key-value bias
+    }
+    model_configs = {
+        "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+        "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+        "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+        "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+    }
+    CHOOSE_MODEL = "gpt2-medium (355M)"
+    BASE_CONFIG.update(model_configs[CHOOSE_MODEL])
+    model_size = CHOOSE_MODEL.split(" ")[-1].lstrip("(").rstrip(")")
+    settings, params = download_and_load_gpt2(
+        model_size=model_size,
+        models_dir="gpt2"
+    )
+    model = GPTModel(BASE_CONFIG)
+    load_weights_into_gpt(model, params)
+    model.eval()
+
+    torch.manual_seed(123)
+    input_text = format_input(val_data[0])
+    # print(input_text)
+
+    token_ids = generate(
+        model=model,
+        idx=text_to_token_ids(input_text, tokenizer),
+        max_new_tokens=35,
+        context_size=BASE_CONFIG["context_length"],
+        eos_id=50256,
+    )
+    generated_text = token_ids_to_text(token_ids, tokenizer)
+    response_text = generated_text[len(input_text):].strip()
+    # print(response_text)
+
+    model.to(device)
+    torch.manual_seed(123)
+    with torch.no_grad():
+        train_loss = calc_loss_loader(
+            train_loader, model, device, num_batches=5
+        )
+    val_loss = calc_loss_loader(
+        val_loader, model, device, num_batches=5
+    )
+    print("Training loss:", train_loss)
+    print("Validation loss:", val_loss)
+
+    start_time = time.time()
+    torch.manual_seed(123)
+    optimizer = torch.optim.AdamW(
+        model.parameters(), lr=0.00005, weight_decay=0.1
+    )
+    num_epochs = 2
+    train_losses, val_losses, tokens_seen = train_model_simple(
+        model, train_loader, val_loader, optimizer, device,
+        num_epochs=num_epochs, eval_freq=5, eval_iter=5,
+        start_context=format_input(val_data[0]), tokenizer=tokenizer
+    )
+    end_time = time.time()
+    execution_time_minutes = (end_time - start_time) / 60
+    print(f"Training completed in {execution_time_minutes:.2f} minutes.")
+
+    epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+    plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+
+    torch.manual_seed(123)
+    for entry in test_data[:3]:  # 1
+        input_text = format_input(entry)
+    token_ids = generate(  # 2
+        model=model,
+        idx=text_to_token_ids(input_text, tokenizer).to(device),
+        max_new_tokens=256,
+        context_size=BASE_CONFIG["context_length"],
+        eos_id=50256
+    )
+    generated_text = token_ids_to_text(token_ids, tokenizer)
+    response_text = (
+        generated_text[len(input_text):]
+        .replace("### Response:", "")
+        .strip()
+    )
+    print(input_text)
+    print(f"\nCorrect response:\\n>> {entry['output']}")
+    print(f"\nModel response:\\n>> {response_text.strip()}")
+    print("-------------------------------------")
+
+    for i, entry in tqdm(enumerate(test_data), total=len(test_data)):
+        input_text = format_input(entry)
+    token_ids = generate(
+        model=model,
+        idx=text_to_token_ids(input_text, tokenizer).to(device),
+        max_new_tokens=256,
+        context_size=BASE_CONFIG["context_length"],
+        eos_id=50256
+    )
+    generated_text = token_ids_to_text(token_ids, tokenizer)
+    response_text = (
+        generated_text[len(input_text):]
+        .replace("### Response:", "")
+        .strip()
+    )
+    test_data[i]["model_response"] = response_text
+    with open("instruction-data-with-response.json", "w") as file:
+        json.dump(test_data, file, indent=4)
+    file_name = f"{re.sub(r'[ ()]', '', CHOOSE_MODEL)}-sft.pth"  # 1
+    torch.save(model.state_dict(), file_name)
+    print(f"Model saved as {file_name}")
--- config.py
+++ config.py
@@ -3,8 +3,10 @@ from typing import List
 import numpy as np
 import tiktoken
 import torch
+from matplotlib import pyplot as plt
+from matplotlib.ticker import MaxNLocator
 from tiktoken import Encoding
-from torch import nn, Tensor
+from torch import nn, Tensor, device
 from torch.nn import Linear, Dropout
 from torch.utils.data import Dataset, DataLoader
 
@@ -292,3 +294,143 @@ def assign(left, right):
     if left.shape != right.shape:
         raise ValueError(f'shape mismatch. Left:{left.shape}, Right:{right.shape}')
     return torch.nn.Parameter(torch.tensor(right))
+
+
+def generate(model, idx, max_new_tokens, context_size, temperature=0.0, top_k=None, eos_id=None):
+    # For-loop is the same as before: Get logits, and only focus on last time step
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+
+        # New: Filter logits with top_k sampling
+        if top_k is not None:
+            # Keep only top_k values
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(logits < min_val, torch.tensor(float("-inf")).to(logits.device), logits)
+
+        # New: Apply temperature scaling
+        if temperature > 0.0:
+            logits = logits / temperature
+
+            # Apply softmax to get probabilities
+            probs = torch.softmax(logits, dim=-1)  # (batch_size, context_len)
+
+            # Sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)  # (batch_size, 1)
+
+        # Otherwise same as before: get idx of the vocab entry with the highest logits value
+        else:
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)  # (batch_size, 1)
+
+        if idx_next == eos_id:  # Stop generating early if end-of-sequence token is encountered and eos_id is specified
+            break
+
+        # Same as before: append sampled index to the running sequence
+        idx = torch.cat((idx, idx_next), dim=1)  # (batch_size, num_tokens+1)
+
+    return idx
+
+
+def calc_loss_loader(data_loader, model: GPTModel, device: device, num_batches: int = None):
+    total_loss: float = 0.
+    if len(data_loader) == 0:
+        return float('nan')
+    elif num_batches is None:
+        num_batches: int | None = len(data_loader)
+    else:
+        num_batches: int | None = min(num_batches, len(data_loader))
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss: Tensor = calc_loss_batch(
+                input_batch, target_batch, model, device
+            )
+            total_loss += loss.item()
+        else:
+            break
+    return total_loss / num_batches
+
+
+def calc_loss_batch(input_batch: Tensor, target_batch: Tensor, model: GPTModel, device: device):
+    input_batch: Tensor = input_batch.to(device)
+    target_batch: Tensor = target_batch.to(device)
+    logits: Tensor = model(input_batch)
+    loss: Tensor = torch.nn.functional.cross_entropy(
+        logits.flatten(0, 1), target_batch.flatten()
+    )
+    return loss
+
+
+def train_model_simple(model: GPTModel, train_loader: DataLoader, val_loader: DataLoader,
+                       optimizer: torch.optim.Optimizer, device: device, num_epochs: int, eval_freq: int,
+                       eval_iter: int, start_context: str, tokenizer: Encoding):
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+    for epoch in range(num_epochs):
+        model.train()
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(
+                input_batch, target_batch, model, device
+            )
+            loss.backward()
+            optimizer.step()
+            tokens_seen += input_batch.numel()
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f'Ep {epoch + 1} (Step {global_step:06d}): Train loss {train_loss:.3f}, Val loss {val_loss:.3f}')
+        generate_and_print_sample(model, tokenizer, device, start_context)
+    return train_losses, val_losses, track_tokens_seen
+
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(
+            train_loader, model, device, num_batches=eval_iter
+        )
+        val_loss = calc_loss_loader(
+            val_loader, model, device, num_batches=eval_iter
+        )
+        model.train()
+        return train_loss, val_loss
+
+
+def generate_and_print_sample(model, tokenizer, device, start_context):
+    model.eval()
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        token_ids = generate_text_simple(
+            model=model,
+            idx=encoded,
+            max_new_tokens=50,
+            context_size=context_size
+        )
+        decoded_text = token_ids_to_text(token_ids, tokenizer)
+        print(decoded_text.replace('\n', " "))
+        model.train()
+
+
+def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+    ax1.plot(epochs_seen, train_losses, label='Training loss')
+    ax1.plot(epochs_seen, val_losses, linestyle='-.', label='Validation loss')
+    ax1.set_xlabel('Epochs')
+    ax1.set_ylabel('Loss')
+    ax1.legend(loc='upper right')
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax2 = ax1.twiny()
+    ax2.plot(tokens_seen, train_losses, alpha=0)
+    ax2.set_xlabel('Epochs')
+    fig.tight_layout()
+    plt.show()
